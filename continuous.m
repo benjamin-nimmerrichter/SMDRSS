df3089123d994e4b813ed2ca079fb295f6fbb12b
benjamin-nimmerrichter <benjamin00@seznam.cz>
2025-04-26 23:04:51 +0200

    Started adding multi channel functionality

--- continuous.m
+++ continuous.m
@@ -1,12 +1,20 @@
 %% --- Continuous measurement for acoustic sound sources.
+% ** not needed for script version
+
+headroomDB = 6;
+numclips = 20;
 aPR = audioPlayerRecorder; % create audio player recorder object
 fs = 44100; % sample rate of the recording
-f = 1000; % beep frequency
+f = 500; % beep frequency
 bufferSz = 1024; % samples
 bufferT = 5; % seconds
 minT = 0.1; % min recording time seconds
 beepT = 0.2; % seconds
 threshold = 0.01; % threshold to start recording
+mics = 1;
+
+% ** not needed for script version
+
 bufferAmt = (ceil(fs/bufferSz))*bufferT; % amount of buffers needed
 minbufferAmt = (ceil(fs/bufferSz))*minT; % minimum amout of buffers to be a recording
 n = linspace (0,round(fs*beepT),round(fs*beepT)); % number of samples for beep
@@ -27,27 +35,29 @@ STOPind = 0;
 while recording == true
     % record buffers 
     % record and play
-    
     outBuffer = aPR(inBuffer);
+
     % add samples to main circular buffer
-    mainBuffer(currentind,:) = outBuffer(:);
+    mainBuffer(currentind,:) = outBuffer(:,:);
     currentind = currentind + 1;
     if currentind > bufferAmt
         currentind = 1;
     end
-    
-    rms_vals = calc_rms2(outBuffer);
-    % if RMS rises, do prerecording step
-    max_rms = max(rms_vals);
-    if started == false
-        if  max_rms > threshold
-            started = true;
-            inind = currentind-1;
-            if inind < 1
-                inind = bufferAmt;
-            end
-            
+
+    for mic = 1:mics
+        rms_vals(mics) = calc_rms2(outBuffer);
+        % if RMS rises, do prerecording step
+        max_rms = max(rms_vals);
+        max_rms = max(max_rms);
+        if started == false
+            if max_rms > threshold
+                started = true;
+                inind = currentind-1;
+                if inind < 1
+                    inind = bufferAmt;
+                end
                 STOPind = mod(inind + bufferAmt-1, bufferAmt)+1;
+            end
         end
     end
     
@@ -69,6 +79,7 @@ while recording == true
     if inind && outind
     end
 end
+meas_out = reshape(mainBuffer(inind:outind,:),1,[]);
 
-output = reshape(mainBuffer(inind:outind,:),1,[]);
-
+% TODO: Multi channel
+% TODO: Clip detect
